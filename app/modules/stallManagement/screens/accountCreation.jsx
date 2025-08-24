2b1d4d1274b9a6699a5d3420787049d7b57697ad
Joseph Martin <se.josephmartin@gmail.com>
2025-08-24 14:03:55 +0800

    update route

--- app/modules/stallManagement/screens/accountCreation.jsx
+++ app/modules/stallManagement/screens/accountCreation.jsx
@@ -37,7 +37,7 @@ export default function AccountCreation() {
   // Function to handle logout
   const handleLogout = () => {
     // Navigate back to the login page
-    router.push("/");
+    router.push("/screens/loginScreen");
   };
 
   const handleCreateAccount = () => {
